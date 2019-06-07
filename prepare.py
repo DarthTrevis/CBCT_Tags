a78a6a12f75e3ad8413bd607bd8b47c88ff21bad
Florian Charlier <florian.charlier@ulb.be>
2019-06-07 09:36:15 +0200

    prepare escapes newlines and export 2 csvs

--- prepare.py
+++ prepare.py
@@ -8,10 +8,15 @@ import csv
 # todo: integrate with other files in order to export work list per reason.
 
 
-data = pd.read_csv('rad_seance.csv', sep=";", quotechar="\"",
-                   names=["treatment", "date", "time", "comment"],
-                   dtype=str, header=0)
+def my_repr(string):
+    return repr(string)[1:-1]
 
+
+data = pd.read_csv('rad_seance_orig.csv', sep=";", quotechar="\"",
+                   names=["treatment", "date", "time", "comment"],
+                   dtype=str, header=0, na_filter=False, escapechar="\\")
+
+data.comment = data.comment.apply(my_repr)
 data["id_date"] = data.treatment.str.cat(data.date)
 
 not_uniques = set(data.groupby("id_date").filter(lambda x: len(x) > 1).id_date)
@@ -20,4 +25,6 @@ data_duplicated_only = data.loc[data.id_date.apply(lambda s: s in not_uniques),
                                 data.columns[0:4]]
 
 data_duplicated_only.to_csv("ok_cols_start.csv", sep=",",
+                            quoting=csv.QUOTE_ALL, index=False)
+data_duplicated_only.to_csv("ok_cols.csv", sep=",",
                             quoting=csv.QUOTE_ALL, index=False)
